71621a0c5b77105914fc3acff0e305196f4d5299
kamrul Hasan <kakamrul2000@gmail.com>
2024-05-03 23:32:03 +0600

    product

---
+++ lib/models/product.dart
@@ -0,0 +1,16 @@
+class Product {
+  final int id;
+  final String name, category, image, description;
+  final double price;
+  int quantity;
+  
+  Product ({
+    required this.id,
+    required this.name,
+    required this.category,
+    required this.price,
+    required this.description,
+    required this.image,
+    required this.quantity,
+  })
+}
